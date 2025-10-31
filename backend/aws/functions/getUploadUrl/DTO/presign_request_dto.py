f843ab784533fc75c65a431dbc69bea72bbe145c
jcarr89532 <90280600+jcarr89532@users.noreply.github.com>
2025-10-31 10:49:03 -0700

    Delete backend/aws/functions/getUploadUrl directory

--- backend/aws/functions/getUploadUrl/DTO/presign_request_dto.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class PresignRequest:
-    filename: str
-    prefix: str = "receipts"
-    expires: int = 300
--- backend/aws/functions/getUploadUrl/DTO/presign_response_dto.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class PresignResponse:
-    url: str
-    bucket: str
-    key: str
-    contentType: str
-    expiresIn: int
